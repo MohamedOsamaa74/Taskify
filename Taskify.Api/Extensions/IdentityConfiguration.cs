57f86581e107037798c6317a957257f01b619968
Mohamed Osama <Mohamed.Elsayed22@svu.edu.eg>
2026-02-12 14:20:56 +0200

    Added IdentityConfiguration Class. Registered ApplicationDbContext and Identity services in Program.cs. added connection string to appsettings.

---
+++ Taskify.Api/Extensions/IdentityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Taskify.Domain.Entities;
+using Taskify.Infrastructure.Contexts;
+
+namespace Taskify.Api.Extensions
+{
+    public static class IdentityConfiguration
+    {
+        public static IServiceCollection ConfigureIdentity(IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 8;
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
--- Taskify.Api/Program.cs
+++ Taskify.Api/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Taskify.Api.Extensions;
+using Taskify.Infrastructure.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+IdentityConfiguration.ConfigureIdentity(builder.Services);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
