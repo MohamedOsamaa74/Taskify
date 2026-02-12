179268648a5d149d513429dfd2ab0ff9335fb2d2
Mohamed Osama <Mohamed.Elsayed22@svu.edu.eg>
2026-02-12 14:22:43 +0200

    Introduced ApplicationDbContext class with Identity integration and DbSets for core entities. added an init migration

---
+++ Taskify.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Taskify.Domain.Entities;
+
+namespace Taskify.Infrastructure.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> users { get; set; }
+        public DbSet<ApplicationRole> roles { get; set; }
+        public DbSet<TaskItem> TaskItems { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<ToDoList> ToDoLists { get; set; }
+        public DbSet<UserTeam> UserTeams { get; set; }
+
+    }
+}
