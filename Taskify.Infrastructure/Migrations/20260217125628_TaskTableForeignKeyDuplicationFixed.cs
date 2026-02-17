1678d78e55e2184ab6de364115c6a95f7242daea
Mohamed Osama <Mohamed.Elsayed22@svu.edu.eg>
2026-02-17 16:21:24 +0200

    Fix TaskItems FK duplication and enforce ToDoList relation
    Removed redundant ListId from TaskItems

---
+++ Taskify.Infrastructure/Migrations/20260217125628_TaskTableForeignKeyDuplicationFixed.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Taskify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskTableForeignKeyDuplicationFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_ToDoLists_ToDoListId",
+                table: "TaskItems");
+
+            migrationBuilder.DropColumn(
+                name: "ListId",
+                table: "TaskItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ToDoListId",
+                table: "TaskItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_ToDoLists_ToDoListId",
+                table: "TaskItems",
+                column: "ToDoListId",
+                principalTable: "ToDoLists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_ToDoLists_ToDoListId",
+                table: "TaskItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ToDoListId",
+                table: "TaskItems",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ListId",
+                table: "TaskItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_ToDoLists_ToDoListId",
+                table: "TaskItems",
+                column: "ToDoListId",
+                principalTable: "ToDoLists",
+                principalColumn: "Id");
+        }
+    }
+}
